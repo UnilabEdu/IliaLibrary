32290f07c14c5f1221accf5aceb00fd86d640100
Irakli00 <iraklitsurtsumia@gmail.com>
2025-06-15 17:09:10 +0400

    page-flip bugfix

--- src/static/js/flip-page.js
+++ src/static/js/flip-page.js
@@ -144,43 +144,48 @@ function setupNavigation() {
   const prevBtnMobile = document.getElementById("prev-mobile");
 
   const handleNext = () => {
-    if (state.currentPage < state.pageFlip.getPageCount() - 1) {
-      if (state.currentPage % 2 !== 0 && state.currentPage !== 0) {
-        state.currentPage += 2;
-      } else {
-        state.currentPage++;
-      }
-      updatePageCountUI();
-      state.pageFlip.flip(state.currentPage);
-      changeChapterHeaderText(state.currentPage, chapterToPageMap);
+    const isLastPage = state.currentPage >= state.pageFlip.getPageCount() - 1;
+
+    if (isLastPage) return;
+
+    if (!state.isMobile && state.currentPage !== 0) {
+      state.currentPage += 2;
+    } else if (state.isMobile) {
+      state.currentPage++;
     }
+
+    updatePageCountUI();
+    state.pageFlip.flip(state.currentPage);
+    changeChapterHeaderText(state.currentPage, chapterToPageMap);
   };
 
   const handlePrev = () => {
-    if (state.currentPage > 0) {
-      if (state.currentPage % 2 == 0 && state.currentPage !== 0) {
-        state.currentPage -= 2;
-      } else {
-        state.currentPage -= 1;
-      }
-      updatePageCountUI();
-      state.pageFlip.flip(state.currentPage);
+    if (state.currentPage <= 0) return;
 
-      changeChapterHeaderText(state.currentPage, chapterToPageMap);
+    if (!state.isMobile) {
+      state.currentPage -= 2;
+    } else {
+      state.currentPage -= 1;
     }
+
+    updatePageCountUI();
+    state.pageFlip.flip(state.currentPage);
+
+    changeChapterHeaderText(state.currentPage, chapterToPageMap);
   };
 
-  const updatePageCountUI = () => {
+  function updatePageCountUI() {
     state.currentPageElement.innerText = state.currentPage;
     state.currentPageElementMobile.innerText = state.currentPage;
-  };
+  }
 
   nextBtn.addEventListener("click", handleNext, { passive: true });
   nextBtnMobile.addEventListener("click", handleNext, { passive: true });
   prevBtn.addEventListener("click", handlePrev, { passive: true });
   prevBtnMobile.addEventListener("click", handlePrev, { passive: true });
 
-  state.pageFlip.on("flip", () => {
+  state.pageFlip.on("flip", (event) => {
+    state.currentPage = event.data;
     updatePageCountUI();
     changeChapterHeaderText(state.currentPage, chapterToPageMap);
   });
@@ -306,7 +311,7 @@ async function initializeViewer(pdfUrl) {
       startZIndex: 0,
       minWidth: 300,
       maxWidth: 1000,
-      useMouseEvents: !state.isMobile,
+      useMouseEvents: true,
       swipeDistance: 30,
       preventTouchEvents: false,
     });
@@ -345,8 +350,6 @@ function updateZoom() {
   zoomLevel > 1
     ? (document.getElementById("pdf-container").style.overflow = "scroll")
     : (document.getElementById("pdf-container").style.overflow = null);
-
-  console.log(document.getElementById("pdf-container").style.overflow);
 
   document.querySelector(".stf__block").style.transform = `scale(${zoomLevel})`;
 }
