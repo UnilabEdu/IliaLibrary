374b2397d020cda2eed564d082b669d4d0bd39b5
Irakli00 <iraklitsurtsumia@gmail.com>
2025-08-20 16:52:38 +0400

    basic accordeon

--- src/static/js/book-details.js
+++ src/static/js/book-details.js
@@ -6,19 +6,19 @@ const featureSection = document.querySelector(".feature-section");
 const searchingSection = document.querySelector(".searching-section");
 const dots = document.querySelectorAll(".dotted-line");
 
-const btns = [anotationBtn, featureBtn, searchBtn];
-btns[0].classList.add("focused");
+const buttons = [anotationBtn, featureBtn, searchBtn];
+buttons[0].classList.add("focused");
 
-btns.forEach((el, i) => {
-  let k = ["anotation", "feature", "search"];
+buttons.forEach((button, i) => {
+  let keys = ["anotation", "feature", "search"];
 
-  el.addEventListener("click", (e) => {
-    btns.forEach((el) => {
-      el.classList.remove("focused");
+  button.addEventListener("click", (e) => {
+    buttons.forEach((button) => {
+      button.classList.remove("focused");
     });
 
     e.target.classList.add("focused");
-    openBookInfo(k[i]);
+    openBookInfo(keys[i]);
   });
 });
 
@@ -45,3 +45,60 @@ function openBookInfo(section) {
     searchingSection.style.opacity = "1";
   }
 }
+
+// ---------- Mobile -------------
+const mobileAnotationBtn = document.getElementById("mobile-anotation-btn");
+const mobileSearchBtn = document.getElementById("mobile-search-btn");
+const mobileFeatureBtn = document.getElementById("mobile-feature-btn");
+const mobileAnotationSection = document.querySelector(
+  ".mobile-anotation-section"
+);
+const mobileFeatureSection = document.querySelector(".mobile-feature-section");
+const mobileSearchingSection = document.querySelector(
+  ".mobile-searching-section"
+);
+const mobileButtons = [mobileAnotationBtn, mobileFeatureBtn, mobileSearchBtn];
+mobileButtons[0].classList.add("focused");
+
+mobileButtons.forEach((button, i) => {
+  let keys = ["anotation", "feature", "search"];
+
+  button.addEventListener("click", (e) => {
+    const key = keys[i];
+    let section = null;
+
+    if (key === "anotation") {
+      section = mobileAnotationSection;
+    } else if (key === "feature") {
+      section = mobileFeatureSection;
+    } else {
+      section = mobileSearchingSection;
+    }
+
+    const isOpen = section.classList.contains("open");
+
+    [
+      mobileAnotationSection,
+      mobileFeatureSection,
+      mobileSearchingSection,
+    ].forEach((sec) => {
+      sec.classList.remove("open");
+      sec.style.maxHeight = "0";
+      sec.style.opacity = "0";
+    });
+
+    mobileButtons.forEach((btn) => {
+      btn.classList.remove("focused");
+      btn.querySelector(".dropdown-icon").classList.remove("rotated");
+    });
+
+    if (!isOpen) {
+      section.classList.add("open");
+      section.style.maxHeight = section.scrollHeight + "px";
+      section.style.opacity = "1";
+
+      e.currentTarget.classList.add("focused");
+      e.currentTarget.querySelector(".dropdown-icon").classList.add("rotated");
+    }
+  });
+});
